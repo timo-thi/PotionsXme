231977702690f40d63ae25c586f8f3d158c89357
Splash Mind <85194871+timo-thi@users.noreply.github.com>
2023-09-07 11:26:12 +0200

    black formatting

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def extract_rule_data(file_path: str) -> dict:
     Returns:
         rule (dict): the rule that has been extracted
     """
-    with open(file_path, 'r') as input_file:
+    with open(file_path, "r") as input_file:
         rule_data = json.load(input_file)
 
     return rule_data
@@ -43,56 +43,53 @@ def evaluate_rule(rule: Any, context: dict = None) -> Any:
     if isinstance(rule, (bool, int, str)):
         return rule
     elif isinstance(rule, dict):
-        if 'and' in rule:
+        if "and" in rule:
             # return true if all subrules are true
-            return all(
-                evaluate_rule(subrule, context) for subrule in rule['and']
-            )
-        elif 'var' in rule:
+            return all(evaluate_rule(subrule, context) for subrule in rule["and"])
+        elif "var" in rule:
             # eval() is used to convert a point separated path like
             # path.to.goal in json path [path][to][goal] to use it on context
-            return eval(
-                "context['"+"']['".join(rule['var'][0].split('.'))+"']"
-            )
-        elif 'increment' in rule:
+            return eval("context['" + "']['".join(rule["var"][0].split(".")) + "']")
+        elif "increment" in rule:
             # same as var rule with an increment
             return 1 + eval(
-                "context['"+"']['".join(rule['increment'][0].split('.'))+"']"
-                )
-        elif 'equals' in rule:
+                "context['" + "']['".join(rule["increment"][0].split(".")) + "']"
+            )
+        elif "equals" in rule:
             # works if there are only two values to compare
-            return evaluate_rule(rule['equals'][0], context) == \
-                evaluate_rule(rule['equals'][1], context)
-        elif 'lower_than' in rule:
+            return evaluate_rule(rule["equals"][0], context) == evaluate_rule(
+                rule["equals"][1], context
+            )
+        elif "lower_than" in rule:
             # works if there are only two values to compare
-            return evaluate_rule(rule['lower_than'][0], context) < \
-                evaluate_rule(rule['lower_than'][1], context)
-        elif 'prop' in rule:
+            return evaluate_rule(rule["lower_than"][0], context) < evaluate_rule(
+                rule["lower_than"][1], context
+            )
+        elif "prop" in rule:
             # prop can't search for a subrule, context don't contain them
-            return context[rule['prop'][0]]
-        elif 'filter' in rule:
+            return context[rule["prop"][0]]
+        elif "filter" in rule:
             # filter rule compare each item in a list of items
             return [
-                item for item in evaluate_rule(rule['filter'][0], context)
-                if evaluate_rule(rule['filter'][1], item)
+                item
+                for item in evaluate_rule(rule["filter"][0], context)
+                if evaluate_rule(rule["filter"][1], item)
             ]
-        elif 'if' in rule:
+        elif "if" in rule:
             # if first rule true, function returns second statement, else third
-            if evaluate_rule(rule['if'][0], context):
-                return evaluate_rule(rule['if'][1], context)
-            return evaluate_rule(rule['if'][2])
+            if evaluate_rule(rule["if"][0], context):
+                return evaluate_rule(rule["if"][1], context)
+            return evaluate_rule(rule["if"][2])
     else:
         raise ValueError("Invalid rule")
 
 
 def main():
-    """Main function, this the only function that should be called
-    """
+    """Main function, this the only function that should be called"""
     for input in inputs_paths:
         rule_data = extract_rule_data(input)
         result = evaluate_rule(
-            rule_data["input"]["rule"],
-            rule_data["input"]["context"]
+            rule_data["input"]["rule"], rule_data["input"]["context"]
         )
 
         print(f"Path : {input}")
